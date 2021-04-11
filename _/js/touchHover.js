02058e07532e5dce7a4cc4dac69afe0c4be8f0fe
vandaillyes <vanda.illyes@gmail.com>
2021-04-11 13:55:56 +0200

    No fadeout, reverted positioning

--- _/js/touchHover.js
+++ _/js/touchHover.js
@@ -32,9 +32,9 @@ $(document).ready(function() {
 		$(this).after('<span class="tooltip">' + $(this).data('title') + '</span>');
 		
 		// position the tooltip 4 pixels above and 4 pixels to the left of the abbreviation
-		var right = $(this).position().right + $(this).width() + 4;
+		var left = $(this).position().left + $(this).width() + 4;
 		var top = $(this).position().top - 4;
-		$(this).next().css('right',right);
+		$(this).next().css('left',left);
 		$(this).next().css('top',top);				
 		
 	});
@@ -49,7 +49,7 @@ $(document).ready(function() {
 		
 		// after a slight 2 second fade, fade out the tooltip for 1 second
 		$(this).next().animate({opacity: 1},{duration: 60000, complete: function(){
-			$(this).fadeOut(1000);
+			//$(this).fadeOut(1000);
 		}});
 		
 	});
